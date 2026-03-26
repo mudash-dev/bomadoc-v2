a12b4c6fe76e649539e2aeae6de0959b7e2b03de
mudash-dev <ashleymudambaji@gmail.com>
2026-03-26 10:34:27 +0300

    Feature Auth: Set up Clerk for Authentication and Middleware

---
+++ proxy.ts
@@ -0,0 +1,28 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { request } from "http";
+
+//list of public routes
+const isPublicRoute = createRouteMatcher([
+    '/',
+    '/sign-in).*)',
+    '/sign-up(.*)',
+    '/api/webhook/clerk'
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+    if(!isPublicRoute(request)){
+
+        // this protects routes like Doctor/Patient dashbpoards
+        await auth.protect();
+    }
+
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files (images, favicon, etc.)
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    
+    '/(api|trpc)(.*)',
+  ],
+};
